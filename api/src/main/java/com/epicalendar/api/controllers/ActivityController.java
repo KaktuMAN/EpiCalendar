9309a6a9949942d10e33dfda1607957e78a5df7e
Nicolas <nicolas.pechart@epitech.eu>
2024-04-11 11:21:04 +0200

    :bug: Allow all CORS

--- api/src/main/java/com/epicalendar/api/controllers/ActivityController.java
+++ api/src/main/java/com/epicalendar/api/controllers/ActivityController.java
@@ -25,7 +25,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "https://intra.epitech.eu")
+@CrossOrigin(origins = "*")
 @Tag(name = "Activity", description = "Activity API")
 @RestController
 @RequestMapping("/activity")
