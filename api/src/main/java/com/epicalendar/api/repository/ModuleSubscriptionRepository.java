9d2b78555c936f196d345488f4685e901e52c075
Nicolas <nicolas.pechart@epitech.eu>
2024-10-18 14:17:13 +0200

    :fire: Removed Project / Module subscriptions

--- api/src/main/java/com/epicalendar/api/repository/ModuleSubscriptionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epicalendar.api.repository;
-
-import com.epicalendar.api.model.ModuleSubscription;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ModuleSubscriptionRepository extends JpaRepository<ModuleSubscription, Integer> {
-}
